1eb1f7218f495ca258724ce32524efb9ab2eb229
Jacob Figueroa <Jacob@Jacobs-MacBook-Pro.local>
2013-10-07 18:41:51 -0500

    Initial Commit

---
+++ 3334 Phase 2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  3334 Phase 2
+//
+//  Created by Jacob Figueroa on 10/7/13.
+//  Copyright (c) 2013 Jacob. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
